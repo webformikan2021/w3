4383182930880d315c08ec562fcc2a333db13308
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2021-12-02 11:43:50 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
+    myImage.setAttribute ('src','images/tsudauni.jng');
   } else {
     myImage.setAttribute ('src','images/firefox-icon.png');
   }
