d9f49e5c9bf2f56f47d87ba7ada6502eca5f87aa
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2021-12-02 11:23:24 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    myHeading.innerHTML = 'こんにちは, ' + myName;
   }
 }
 
