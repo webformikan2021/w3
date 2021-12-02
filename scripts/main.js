20afbc43263d6ca842066b567011173ac8c6ed1d
webformikan2021 <95006565+webformikan2021@users.noreply.github.com>
2021-12-02 11:26:29 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName; 
 }
 
 myButton.onclick = function() {
